6fe9aa823748bf2ca0f4d64b002d4a4731ab4f8a
AwatefVicente <vawatef@hotmail.fr>
2022-10-18 17:03:10 +0200

    partie 1 finie

--- app.rb
+++ app.rb
@@ -6,26 +6,26 @@ Bundler.require
 require_relative 'lib/player'
 
 player1 = Player.new("Josiane")
-player2 =Player.new("José")
+player2 = Player.new("José")
 
-puts "Voici l'état des joueurs :"
-puts player1.show_state
-puts player2.show_state
-
-
-
-while player1.life_points > 0 && player2.life_points > 0 do
+while player1.life_points > 0 && player2.life_points > 0
+  
+  puts "Voici l'état des joueurs :"
+  puts player1.show_state
+  puts player2.show_state
+  print "\n"
+  
   puts "3, 2, 1 let's  go fiiiiiight !"
-  player2.attacks(player1)
-  player1.life_points > 0 ? 
+  puts player1.attacks(player2) 
 
-  player1.attacks(player2)
+  if player2.life_points <= 0
+    break
+  end
+  puts player2.attacks(player1)
 
-  break
+  print "\n"
 
 end
-
-
 
 
 binding.pry
